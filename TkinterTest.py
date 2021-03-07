db09da908fd6640fdb83a01cc6e4de8d0d457db3
Juliaququ <57101232+Juliaququ@users.noreply.github.com>
2021-03-07 13:38:57 +0800

    输入判断已解决

--- TkinterTest.py
+++ TkinterTest.py
@@ -8,25 +8,22 @@ window = tk.Tk()
 window.title("DegreeSquence")
 window.geometry('500x300')
 
-e = tk.StringVar()
-e2 = tk.Entry(window, show=None, font=('Arial', 14),textvariable = e)  # 显示成明文形式
-e2.pack()
-
-sequence = e.get()
+tk.Label(window, text='请输入度序列，以逗号间隔，如：7,7,6,6,3,2',font=('Arial', 12), ).place(x=19, y=35)
+userName = tk.StringVar()
+tk.Entry(window, highlightthickness=1, textvariable=userName).place(x=20, y=60, width=320, height=30)
 
 def check_input():
-
-    seq_list = sequence.split(',')
-
+    seq_list = userName.get().split(',')
+    print(seq_list)
     for item in seq_list:
         if (item.isdigit() == False):
             tk.messagebox.showerror(message='输入格式错误')
             break
 
+    pass
 
+b = tk.Button(window, text='确认', font=('Arial', 12), width=10, height=1, command=check_input).place(x = 20,y = 100)
 
-b = tk.Button(window, text='确认', font=('Arial', 12), width=10, height=1,command = check_input)
-b.pack()
 
 # 第6步，主窗口循环显示
 window.mainloop()  # 消息循环是什么意思？
